2b7a196243c23eb1c8ff9752227a401527ea261e
Phillip Farber <pfarber@umich.edu>
2010-07-09 15:44:09 -0400

    Path changes for the migration to HTDE

--- Auth/Auth.pm
+++ Auth/Auth.pm
@@ -205,7 +205,7 @@ sub get_WAYF_login_href {
     $return_to_url = Utils::url_over_SSL_to($return_to_url);
 
     my $wayf_key = $ENV{'HT_DEV'} ? 'dev_WAYF_url' : 'WAYF_url';
-    my $WAYF_url = $C->get_object('Config')->get($wayf_key);
+    my $WAYF_url = $C->get_object('MdpConfig')->get($wayf_key);
 
     $WAYF_url =~ s,___HOST___,$ENV{'HTTP_HOST'},;
     $WAYF_url .= qq{?target=$return_to_url};
