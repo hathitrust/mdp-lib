a0efffef1c2a2d3cdf6b9cf1ed81c6a9b39b9b72
Phillip Farber <pfarber@umich.edu>
2011-05-02 15:50:11 -0400

    * Debug/DUtils.pm: fix regexp to extract appname from SCRIPT_NAME

--- Debug/DUtils.pm
+++ Debug/DUtils.pm
@@ -248,7 +248,11 @@ fix up paths to css and graphics in static HTML files.
 
 # ---------------------------------------------------------------------
 sub ___determine_app {
-    my ($appname) = ($ENV{SCRIPT_NAME} =~ m,^/(.*?)/,);
+
+    # regex to capture in $1 e.g. 'pt' in either /cgi/pt or
+    # /cgi/pt/search namely: anyting after /cgi/ that is followed by a
+    # slash but not the slash or everything after /cgi/
+    my ($appname) = ($ENV{SCRIPT_NAME} =~ m,^/cgi/((.*)(?=/)|(.*)),);
     return $appname;
 }
 
