67e1a0f8a5aacc6c62ef4eaa4f315e383376a094
Roger Espinosa <roger@umich.edu>
2011-09-06 13:23:16 -0400

    add users for orphan works project

--- MdpUsers.pm
+++ MdpUsers.pm
@@ -358,6 +358,33 @@ $gStaffExpireDate = '2011-12-31 23:59:59';
                         ],
 
       },
+      
+   # Orphan works review
+   'adamsn'
+   => {
+       'displayname' => 'Adams, Neena',
+       'supervisor'  => 'pfarber',
+       'expires'     => $gStaffExpireDate,
+       'usertype'    => 'staff',
+       'iprestrict'  => [
+                         '^141\.211\.173\.204$',
+                         '^141\.211\.173\.205$',
+                        ],
+
+      },
+   
+   'bryanbir'
+   => {
+       'displayname' => 'Birchmeier, Bryan',
+       'supervisor'  => 'pfarber',
+       'expires'     => $gStaffExpireDate,
+       'usertype'    => 'staff',
+       'iprestrict'  => [
+                         '^141\.211\.173\.204$',
+                         '^141\.211\.173\.205$',
+                        ],
+
+      },
 
    # Digitization
    'lwentzel'
