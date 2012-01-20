a493acaf92219cdd8ecf27c83b3ee9ba32ac343d
Phillip Farber <pfarber@umich.edu>
2012-01-20 12:03:38 -0500

    * RightsGlobals.pm: add cc-zero attr=17

--- RightsGlobals.pm
+++ RightsGlobals.pm
@@ -35,6 +35,7 @@ package RightsGlobals;
  13        cc-by-nc    copyright  cc-by +  non-commercial use only
  14        cc-by-nc-sa copyright  cc-by-nc + ccby-sa
  15        cc-by-sa    copyright  cc-by + same license upon redistribution
+ 17        cc-zero     copyright  cc0 license implies pd
 
  (Orphan works project)
 
@@ -57,6 +58,7 @@ package RightsGlobals;
  cc-by-nc-sa
  cc-by-sa
  orphcand
+ cc-zero
 
 =cut
 
@@ -111,6 +113,7 @@ $HT_AFFILIATE           = 5;
    '14' => 'copyright cc-by-nc + ccby-sa',
    '15' => 'copyright cc-by + same license upon redistribution',
    '16' => 'in-copyright orphan candidate',
+   '17' => 'cc0 no rights reserved license implies pd',
   );
 
 %g_attribute_keys =
@@ -207,6 +210,10 @@ $HT_AFFILIATE           = 5;
    'cc-by-sa'     => {
                       'stmt_icon_aux' => 'http://i.creativecommons.org/l/by-sa/3.0/us/80x15.png',
                       'stmt_url_aux'  => 'http://creativecommons.org/licenses/by-sa/3.0/us/',
+                     },
+   'cc-zero'      => {
+                      'stmt_icon_aux' => 'http://i.creativecommons.org/l/zero/1.0/80x15.png',
+                      'stmt_url_aux'  => 'http://creativecommons.org/publicdomain/zero/1.0/',
                      },
    'candidates'   => {
                       'stmt_icon' => '',
@@ -358,13 +365,21 @@ $HT_AFFILIATE           = 5;
               $UM_AFFILIATE          => 'deny',
               $HT_AFFILIATE          => 'deny',
             },
+     # available to everyone in the world http://creativecommons.org/publicdomain/zero/1.0/
+     '17' => { 
+              $ORDINARY_USER         => 'allow',
+              $SSD_USER              => 'allow',
+              $LIBRARY_IPADDR_USER   => 'allow',
+              $UM_AFFILIATE          => 'allow',
+              $HT_AFFILIATE          => 'allow',
+            },
     );
 
 # ---------------------------------------------------------------------
 # "Public domain"
 # ---------------------------------------------------------------------
 #
-@g_creative_commons_attribute_values = (10, 11, 12, 13, 14, 15);
+@g_creative_commons_attribute_values = (10, 11, 12, 13, 14, 15, 17);
 @g_public_domain_world_attribute_values = (1, 7, 9);
 @g_access_requires_holdings_attribute_values = (2, 3, 4, 5, 6, 16);
 
