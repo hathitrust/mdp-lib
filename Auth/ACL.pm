da8f68c297ba6a64eb62ff5708669061cab617f4
Phillip Farber <pfarber@umich.edu>
2014-04-03 14:06:02 -0400

    * Auth/ACL.pm: updates for locking to single IP plus vpn, added admin fields

--- Auth/ACL.pm
+++ Auth/ACL.pm
@@ -17,40 +17,46 @@ The database section of the package is an interface to
 ht_repository.ht_users.  Certain DEBUG switches are implemented
 here and override the ACL.
 
+   CREATE TABLE `ht_users` (
+      `userid`      varchar(256)  NOT NULL DEFAULT '',
+      `displayname` varchar(128)           DEFAULT NULL,
+      `email`       varchar(128)           DEFAULT NULL,
+      `supemail`    varchar(128)           DEFAULT NULL,
+      `approver`    varchar(128)           DEFAULT NULL,
+      `authorizer`  varchar(128)           DEFAULT NULL,
+      `usertype`    varchar(32)            DEFAULT NULL,
+      `role`        varchar(32)            DEFAULT NULL,
+      `access`      varchar(32)            DEFAULT 'normal',
+      `expires`     timestamp     NOT NULL DEFAULT '0000-00-00 00:00:00',
+      `iprestrict`  varchar(1024)          DEFAULT NULL,
+      `vpn`         tinyint(1)    NOT NULL DEFAULT '0',
+      PRIMARY       KEY (`userid`));
+
 Subroutines prefixed with '__' are private and should only be called
 from within the Auth::ACL package.
 
-usertype values are 'staff' (UM), 'student' (UM), 'external' (non-UM)
+'usertype' values are 'staff' (UM), 'student' (UM), 'external' (non-UM)
 
-roles are subclasses of usertype:
-
- CREATE TABLE `ht_users` (
-    `userid`      varchar(256)  DEFAULT NULL,
-    `displayname` varchar(128)  DEFAULT NULL,
-    `usertype`    varchar(32)   DEFAULT NULL,
-    `role`        varchar(32)   DEFAULT NULL,
-    `expires`     varchar(32)   DEFAULT NULL,
-    `iprestrict`  varchar(1024) DEFAULT NULL,
-            PRIMARY KEY (`userid`));
+'role' is a subclass of 'usertype'
 
  SELECT DISTINCT usertype, role FROM ht_users;
 
  +----------+--------------+---------+
  | usertype | role         | access  |
  +----------+--------------+---------+
- | staff    | generalhathi | total   | staff locked to $staff_subnet_ranges
- | staff    | cataloging   | total   | staff locked to $staff_subnet_ranges
- | external | crms         | total   | external engaged in CRMS and CRMS World activities, locked to IP
- | staff    | crms         | total   | staff engaged in CRMS and CRMS World activities, locked to IP or $staff_subnet_ranges
- | staff    | superuser    | total   | staff (developers) locked to $superuser_subnet_ranges
- | staff    | orphan       | total   | staff engaged in the Orphan Works project, locked to IP
- | staff    | quality      | total   | staff engaged in the Qual project, locked to $staff_subnet_ranges
- | external | quality      | total   | external engaged in the Qual project, locked to IP
- | staff    | digitization | total   | staff at DCU, locked to IP
+ | staff    | generalhathi | total   | UM staff
+ | staff    | cataloging   | total   | UM staff
+ | external | crms         | total   | non-UM engaged in CRMS and CRMS World activities
+ | staff    | crms         | total   | UM staff engaged in CRMS and CRMS World activities
+ | staff    | superuser    | total   | UM staff (developers)
+ | staff    | orphan       | total   | UM staff engaged in the Orphan Works project
+ | staff    | quality      | total   | UM staff engaged in the Qual project
+ | external | quality      | total   | non-UM engaged in the Qual project
+ | staff    | digitization | total   | UM staff at DCU
  |----------+--------------+---------+
- | student  | ssd          | normal  | UM SSD student list not locked to by IP range
- | external | ssdproxy     | normal  | external Human Proxy for print-disabled user locked to IP address
- | external | ssdnfb       | normal  | external National Federation of the Blind Proxy for print-disabled user locked to IP address
+ | student  | ssd          | normal  | UM student on SSD list *not locked to any IP address*
+ | external | ssdproxy     | normal  | non-UM Human Proxy for print-disabled user
+ | external | ssdnfb       | normal  | non-UM National Federation of the Blind Proxy for print-disabled user
  +----------+--------------+---------+
 
 'normal' access excludes attr=8 (nobody)
@@ -61,13 +67,13 @@ Coding example
 
 =head1 IP RANGES
 
- Mon Feb 13 2012 Superusers are restricted to these ranges
- 141.211.43.128/25   141.211.43.129  - 141.211.43.254  - LIT offices
- 141.211.84.128/25   141.211.84.129  - 141.211.84.254  - Library VPN - disallowed as of Thu Nov 21 12:21:13 2013
- 141.211.168.128/25  141.211.168.129 - 141.211.168.254 - Hatcher server room
- 141.211.172.0/22    141.211.172.1   - 141.211.175.254 - Hatcher/Shapiro buildings
- 141.213.128.128/25  141.213.128.129 - 141.213.128.254 - MACC data center
-                     141.211.174.173 - 141.211.174.199 - ULIC Shapiro 4th floor
+Wed Apr  2 13:33:34 2014
+
+All users except 'role'='ssd' are locked to one or more single IP
+addresses (no ranges). Users with 'vpn'=1 are also permitted to the
+vpn range
+
+ 141.211.84.128/25   141.211.84.129  - 141.211.84.254  - Library VPN
 
 =head1 METHODS
 
@@ -88,63 +94,19 @@ use DbUtils;
 #
 # The ACL
 #
-my %gAccessControlList;
+my %gAccessControlList = ();
 
-my $lit_offices_range          = q{^(141\.211\.43\.(1(29|[3-9][0-9])|2([0-4][0-9]|5[0-4])))$};
-my $library_vpn_range          = q{^(141\.211\.84\.(1(29|[3-9][0-9])|2([0-4][0-9]|5[0-4])))$};
-my $hatcher_server_room_range  = q{^(141\.211\.168\.(1(29|[3-9][0-9])|2([0-4][0-9]|5[0-4])))$};
-my $hatcher_shapiro_bldg_range = q{^(141\.211\.(1(7[2-5]))\.([1-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-4])))$};
-my $macc_data_center_range     = q{^(141\.213\.128\.(1(29|[3-9][0-9])|2([0-4][0-9]|5[0-4])))$};
-my $ulic_range                 = q{^(141\.211\.174\.(1(7[3-9]|[8-9][0-9])))$};
-
-# superusers
-my @superuser_ranges =
-  (
-   $lit_offices_range,
-   # $library_vpn_range, # per jweise, csnavely Thu Nov 21 12:20:33 2013
-   $hatcher_server_room_range,
-   $hatcher_shapiro_bldg_range,
-   $macc_data_center_range,
-   $ulic_range,
-  );
-my $superuser_subnet_ranges = join('|', @superuser_ranges);
-
-# Staff
-my @staff_ranges =
-  (
-   $hatcher_shapiro_bldg_range,
-   $lit_offices_range,
-  );
-my $staff_subnet_ranges = join('|', @staff_ranges);
-
-my $ULIC_subnet_ranges = $ulic_range;
+my $library_vpn_range = q{^(141\.211\.84\.(1(29|[3-9][0-9])|2([0-4][0-9]|5[0-4])))$};
 
 # blocked
-my $null_range = 'notanipaddress';
+my $iprestrict_all = 'notanipaddress';
 
-# wide open (SSD)
-my $unrestricted_range = '.*';
+# unrestricted (SSD only, after Wed Apr  2 13:42:55 2014)
+my $iprestrict_none = '.*';
 
 my $ZERO_TIMESTAMP = '0000-00-00 00:00:00';
 my $GLOBAL_EXPIRE_DATE = '2014-12-31 23:59:59';
 
-# staff:{other} access expires on date:
-my $staff_expire_date = $GLOBAL_EXPIRE_DATE;
-
-# staff:superuser access expires on date:
-my $superuser_expire_date = $GLOBAL_EXPIRE_DATE;
-
-# student:{other} access expires on date:
-my $student_expire_date = $GLOBAL_EXPIRE_DATE;
-
-# external:{other} access expires on date:
-my $external_expire_date = $GLOBAL_EXPIRE_DATE;
-
-# {all}:ssd* access expires on date:
-my $SSD_expire_date = $GLOBAL_EXPIRE_DATE;
-
-# {all}:crms users access expires on date:
-my $CRMS_expire_date = $GLOBAL_EXPIRE_DATE;
 
 # ---------------------------------------------------------------------
 
@@ -384,10 +346,11 @@ sub __debug_acl {
                   my $role       = $gAccessControlList{$userid}{role};
                   my $access     = $gAccessControlList{$userid}{access};
                   my $iprestrict = $gAccessControlList{$userid}{iprestrict};
+                  my $vpn        = $gAccessControlList{$userid}{vpn};
                   my $expires    = $gAccessControlList{$userid}{expires};
                   my $name       = $gAccessControlList{$userid}{displayname};
 
-                  $s .= qq{<h3 style="text-align:left">ACL DUMP: userid=$userid name=$name expires=$expires type=$usertype role=$role acess=$access <font color="blue">ip=$iprestrict </font></h3>};
+                  $s .= qq{<h3 style="text-align:left">ACL DUMP: userid=$userid name=$name expires=$expires type=$usertype role=$role access=$access vpn=<font color="red">$vpn</font> <font color="blue">ip=$iprestrict </font></h3>};
               }
               $__b_debug_printed = 1;
               return $s;
@@ -462,8 +425,9 @@ sub __load_access_control_list {
     my $statement = qq{SELECT * FROM ht_users};
     my $sth = DbUtils::prep_n_execute($dbh, $statement);
     my $ref_to_arr_of_hashref = $sth->fetchall_arrayref({});
-
+    
     foreach my $hashref (@$ref_to_arr_of_hashref) {
+
         my $userid = $hashref->{userid};
 
         $gAccessControlList{$userid}{userid} = $hashref->{userid};
@@ -472,52 +436,37 @@ sub __load_access_control_list {
         $gAccessControlList{$userid}{role} = $hashref->{role};
         $gAccessControlList{$userid}{access} = $hashref->{access};
 
-        # Stored IP range and expiration date are used, if defined,
-        # else we use one of the above hardcoded ranges depending on
-        # usertype and role.
-        my $iprestrict = $hashref->{iprestrict};
-        if (defined $iprestrict) {
-            $gAccessControlList{$userid}{iprestrict} = $iprestrict;
-        }
+        $gAccessControlList{$userid}{iprestrict} = $hashref->{iprestrict}; 
+        $gAccessControlList{$userid}{vpn} = $hashref->{vpn};
 
+        # Use database IP address(es), if defined. If not defined, use
+        # the "no access" IP address or some other value in special
+        # cases (SSD) below. Add the VPN range if vpn=1
+        #
+        my $vpn = $hashref->{vpn};
+        my $iprestrict = $hashref->{iprestrict};
+
+        if (defined $iprestrict) {
+            $gAccessControlList{$userid}{iprestrict} = ($vpn ? join( '|', ($iprestrict, $library_vpn_range) ) : $iprestrict);
+        }
+        else {
+            $gAccessControlList{$userid}{iprestrict} = ($vpn ? $library_vpn_range : $iprestrict_all);
+        }
+        
         my $expires = $hashref->{expires};
         $expires = ( ($expires eq $ZERO_TIMESTAMP) ? undef : $expires );
         if (defined $expires) {
             $gAccessControlList{$userid}{expires} = $expires;
         }
-
-        if ($gAccessControlList{$userid}{usertype} eq 'staff') {
-            if ($gAccessControlList{$userid}{role} eq 'superuser') {
-                $gAccessControlList{$userid}{expires} = $superuser_expire_date unless (defined $expires);
-                $gAccessControlList{$userid}{iprestrict} = $superuser_subnet_ranges unless (defined $iprestrict);
-            }
-            else {
-                $gAccessControlList{$userid}{expires} = $staff_expire_date unless (defined $expires);
-                $gAccessControlList{$userid}{iprestrict} = $staff_subnet_ranges unless (defined $iprestrict);
-            }
+        else {
+            $gAccessControlList{$userid}{expires} = $GLOBAL_EXPIRE_DATE;
         }
-        elsif ($gAccessControlList{$userid}{usertype} eq 'external') {
-            if ($gAccessControlList{$userid}{role} eq 'crms') {
-                $gAccessControlList{$userid}{expires} = $CRMS_expire_date unless (defined $expires);
-                $gAccessControlList{$userid}{iprestrict} = $null_range unless (defined $iprestrict);
-            }
-            elsif ($gAccessControlList{$userid}{role} =~ m,^ssd,) {
-                $gAccessControlList{$userid}{expires} = $SSD_expire_date unless (defined $expires);
-                $gAccessControlList{$userid}{iprestrict} = $null_range unless (defined $iprestrict);
-            }
-            else {
-                $gAccessControlList{$userid}{expires} = $external_expire_date unless (defined $expires);
-                $gAccessControlList{$userid}{iprestrict} = $null_range unless (defined $iprestrict);
-            }
-        }
-        elsif ($gAccessControlList{$userid}{usertype} eq 'student') {
+        
+        # Special cases
+        #
+        if ($gAccessControlList{$userid}{usertype} eq 'student') {
             if ($gAccessControlList{$userid}{role} eq 'ssd') {
-                $gAccessControlList{$userid}{expires} = $SSD_expire_date unless (defined $expires);
-                $gAccessControlList{$userid}{iprestrict} = $unrestricted_range unless (defined $iprestrict);
-            }
-            else {
-                $gAccessControlList{$userid}{expires} = $student_expire_date unless (defined $expires);
-                $gAccessControlList{$userid}{iprestrict} = $null_range unless (defined $iprestrict);
+                $gAccessControlList{$userid}{iprestrict} = $iprestrict_none;
             }
         }
     }
