c3b73bf3fd1881482e8418b7e75c566b06ac94dd
Roger Espinosa <roger@umich.edu>
2017-11-14 14:51:06 -0500

    logging all requests

--- Auth/Logging.pm
+++ Auth/Logging.pm
@@ -203,6 +203,130 @@ sub _log_access {
     Utils::Logger::__Log_string($C, $s, 'pt_access_logfile', $pattern, 'pt');
 }
 
+sub log_access {
+    my ($C, $app_name, $tuples, $altenv) = @_;
+
+    require URI::Escape;
+
+    my $auth = $C->get_object('Auth');
+    my $mdpItem = $C->get_object('MdpItem', 1);
+    my $session = $C->get_object('Session', 1);
+    $altenv = {} unless ( ref($altenv) );
+
+    my $datetime = Utils::Time::iso_Time();
+
+    my $message = [['datetime', $datetime], ['app', $app_name]];
+
+    if ( ref($mdpItem) ) {
+        # my $s .= qq{$message|app=$app_name|id=$id|datetime=$datetime|attr=$attr|ic=$ic|access_type=$access_type|remote_addr=$remote_addr|proxied_addr=$proxied_addr|request_uri=$request_uri|http_referer=$http_referer|user_agent=$user_agent|remote_user_env=$remote_user_from_env|remote_user_processed=$remote_user_processed|auth_type=$auth_type|usertype=$usertype|role=$role|sdrinst=$sdrinst|sdrlib=$sdrlib|http_host=$http_host|inst_code=$inst_code|inst_code_mapped=$inst_code_mapped|inst_name=$inst_name|inst_name_mapped=$inst_name_mapped|geo_code=$country_code|geo_name=$country_name|geo_code_proxy=$country_code_prox|geo_name_proxy=$country_name_prox};
+
+        my $ar = $C->get_object('Access::Rights');
+        my $id = $mdpItem->GetId();
+        my $attr                  = $ar->get_rights_attribute($C, $id) || 0;
+        my $ic                    = $ar->in_copyright($C, $id) || 0;
+        my $access_type           = $ar->get_access_type($C, 'as_string');
+        push @$message, ['id', $id];
+        push @$message, ['attr', $attr];
+        push @$message, ['ic', $ic];
+        push @$message, ['access', $ar->check_final_access_status($C, $id) eq 'allow' ? 'success' : 'failure'];
+        push @$message, ['access_type', $access_type];
+    }
+
+
+    push @$message,           ['session', $session ? $session->get_session_id() : 'notset'];
+    my $remote_addr           = $ENV{REMOTE_ADDR} || 'notset';
+    my $proxied_addr          = Access::Rights::proxied_address() || 'notset';
+    my $remote_user_processed = Utils::Get_Remote_User() || 'notset';
+    my $remote_user_from_env  = $ENV{REMOTE_USER} || 'notset';
+    my $affiliation           = $ENV{affiliation} || 'notset';
+    my $eppn                  = $ENV{eppn} || 'notset';
+    my $display_name          = $ENV{displayName} || 'notset';
+    my $entityID              = $ENV{Shib_Identity_Provider} || 'notset';
+    my $persistent_id         = $ENV{persistent_id} || 'notset';
+    my ($usertype, $role) = (
+                             Auth::ACL::a_GetUserAttributes('usertype') || 'notset',
+                             Auth::ACL::a_GetUserAttributes('role') || 'notset',
+                            );
+    my $sdrinst               = $ENV{SDRINST} || 'notset';
+    my $remote_realm          = $ENV{REMOTE_REALM} || 'notset';
+    my $auth_type             = lc ($ENV{AUTH_TYPE} || 'notset');
+    my $http_host             = $$altenv{HTTP_HOST} || $ENV{HTTP_HOST} || 'notset';
+    my $sdrlib                = $ENV{SDRLIB} || 'notset';
+    my $http_referer          = $$altenv{HTTP_REFERER} || $ENV{HTTP_REFERER}  || 'notset'; # $http_referer = URI::Escape::uri_escape($http_referer);
+    my $request_uri           = $$altenv{REQUEST_URI} || $ENV{REQUEST_URI}  || 'notset'; # $request_uri = URI::Escape::uri_escape($request_uri);
+    my $user_agent            = $ENV{HTTP_USER_AGENT}  || 'notset';
+    my $inst_code             = $auth->get_institution_code($C) || 'notset';
+    my $inst_code_mapped      = $auth->get_institution_code($C, 1) || 'notset';
+    my $inst_name             = $auth->get_institution_name($C) || 'notset';
+    my $inst_name_mapped      = $auth->get_institution_name($C, 1) || 'notset';
+
+    require Geo::IP;
+    my $geoIp = Geo::IP->new();
+    my $geo_code = $geoIp->country_code_by_addr($remote_addr) || 'notset'; chomp $geo_code;
+    my $geo_name = $geoIp->country_name_by_addr($remote_addr) || 'notset'; chomp $geo_name;
+    my $geo_code_proxy = $geoIp->country_code_by_addr($proxied_addr) || 'notset'; chomp $geo_code_proxy;
+    my $geo_name_proxy = $geoIp->country_name_by_addr($proxied_addr) || 'notset'; chomp $geo_name_proxy;
+
+
+    push @$message, ['remote_addr', $remote_addr];
+    push @$message, ['proxied_addr', $proxied_addr];
+    push @$message, ['remote_user_processed', $remote_user_processed];
+    push @$message, ['remote_user_from_env', $remote_user_from_env];
+    push @$message, ['affiliation', $affiliation];
+    push @$message, ['eppn', $eppn];
+    push @$message, ['display_name', $display_name];
+    push @$message, ['entityID', $entityID];
+    push @$message, ['persistent_id', $persistent_id];
+    push @$message, ['usertype', $usertype];
+    push @$message, ['role', $role];
+    push @$message, ['sdrinst', $sdrinst];
+    push @$message, ['auth_type', $auth_type];
+    push @$message, ['http_host', $http_host];
+    push @$message, ['sdrlib', $sdrlib];
+    push @$message, ['http_referer', $http_referer];
+    push @$message, ['request_uri', $request_uri];
+    push @$message, ['user_agent', $user_agent];
+    push @$message, ['inst_code', $inst_code];
+    push @$message, ['inst_code_mapped', $inst_code_mapped];
+    push @$message, ['inst_name', $inst_name];
+    push @$message, ['inst_name_mapped', $inst_name_mapped];
+    push @$message, ['geo_code', $geo_code];
+    push @$message, ['geo_name', $geo_name];
+    push @$message, ['geo_code_proxy', $geo_code_proxy];
+    push @$message, ['geo_name_proxy', $geo_name_proxy];
+
+    # $s .= qq{|eduPersonScopedAffiliation=$affiliation|eduPersonPrincipalName=$eppn|displayName=$display_name|persistent_id=$persistent_id|Shib_Identity_Provider=$entityID};
+
+
+    my $proxied_addr_hash = Access::Rights::get_proxied_address_data();
+    foreach my $key (keys %$proxied_addr_hash) {
+        my $ip = $proxied_addr_hash->{$key};
+        if ($ip) {
+            push @$message, [$key, $ip];
+        }
+    }
+
+    push @$message, @$tuples if ( ref($tuples) );
+    # my $s = join('|', map { join('=', @$_) } @$message);
+    require JSON::XS;
+    my $json = JSON::XS->new()->utf8(1)->allow_nonref(1);
+    my $s = '{';
+    while ( scalar @$message ) {
+        my $kv = shift @$message;
+        my ( $key, $value ) = @$kv;
+        my $suffix = scalar @$message ? "," : "";
+        $s .= sprintf(qq{%s:%s%s}, $json->encode($key), $json->encode($value), $suffix)
+    }
+    $s .= '}';
+
+    # The optional_dir_pattern "slip/run-___RUN___" is used here
+    # to replace that portion of the logdir set when the slip
+    # config is merged into pt's config to restore the original
+    # logdir path SDRROOT/logs/pt ... what a tangled web we weave
+    my $pattern = qr(slip/run-___RUN___|___QUERY___);
+    Utils::Logger::__Log_string($C, $s, 'access_logfile', $pattern, 'access');
+}
+
 1;
 
 __END__
