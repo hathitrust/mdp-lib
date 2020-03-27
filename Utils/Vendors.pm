7811f33fd14e370237c4291e7e26058f23c7d558
Roger Espinosa <roger@umich.edu>
2020-03-27 16:42:28 -0400

    better handling of debug flags

--- Utils/Vendors.pm
+++ Utils/Vendors.pm
@@ -39,6 +39,7 @@ sub init {
     $local_switch = $local_switch || $ENV{DEBUG_LOCAL} || ($ENV{REQUEST_METHOD} ne 'POST' && CGI::param('debug') && (CGI::param('debug') =~ m,local,));
     if ($local_switch) {
         push(@my_inc, "$ENV{SDRROOT}/mdp-lib");
+        $ENV{DEBUG_LOCAL} = 1;
         $ENV{DEBUG} .= ',' if ( $ENV{DEBUG} );
         $ENV{DEBUG} .= 'local';
     }
@@ -59,7 +60,7 @@ sub init {
         }
     }
 
-    unshift(@INC, @my_inc);    
+    unshift(@INC, @my_inc);
 }
 
 1;
